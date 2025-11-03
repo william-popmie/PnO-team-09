dacd1759d35910f412d44d3a6d10f36dc82c146f
Dirk Nuyens <dirk.nuyens@kuleuven.be>
2025-11-03 12:55:27 +0100

    Make up to date for 2025-2026 (linter not clean due to incomplete code)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,47 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/build/", "**/coverage/"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
     },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: ["./tsconfig.json"],
+        },
+    },
+
     rules: {
         "@typescript-eslint/no-unused-vars": ["warn", {
             argsIgnorePattern: "^_",
             destructuredArrayIgnorePattern: "^_",
         }],
-        "eqeqeq": ["error", "always"],
+
+        eqeqeq: ["error", "always"],
         "guard-for-in": ["warn"],
         "@typescript-eslint/prefer-for-of": ["warn"],
     },
-  },
-);
+}];

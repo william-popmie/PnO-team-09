4777dc769ad1b957e1ee0fc40d879b87a1875c89
Mathias Bouhon Keulen <mathias.bouhonkeulen@student.kuleuven.be>
2026-02-11 20:44:57 +0100

    implementation custom raft errors

--- src/util/Error.ts
+++ src/util/Error.ts
@@ -5,6 +5,84 @@ export class RaftError extends Error {
     ) {
         super(message);
         this.name = 'RaftError';
-        Object.setPrototypeOf(this, RaftError.prototype); // anders kapotte portotype chain 
+        Object.setPrototypeOf(this, RaftError.prototype); // anders kapotte prototype chain 
     }
 }
+
+export class NotLeaderError extends RaftError {
+    constructor(
+        public readonly leaderId: string | null
+    ) {
+        super(
+            leaderId
+                 ? `Not the leader. Current leader is ${leaderId}.`
+                 :'Not the leader. No leader currently known.',
+            'NOT_LEADER'
+        )
+        this.name = 'NotLeaderError';
+        Object.setPrototypeOf(this, NotLeaderError.prototype);
+    }
+}
+
+export class LogInconsistencyError extends RaftError {
+    constructor(
+        message: string
+    ) {
+        super(message, 'LOG_INCONSISTENCY');
+        this.name = 'LogInconsistencyError';
+        Object.setPrototypeOf(this, LogInconsistencyError.prototype);
+    }
+}
+
+export class StorageError extends RaftError {
+    constructor(
+        message: string, public readonly cause?: Error
+    ) {
+        super(message, 'STORAGE_ERROR');
+        this.name = 'StorageError';
+        Object.setPrototypeOf(this, StorageError.prototype);
+    }
+}
+
+export class NetworkError extends RaftError {
+    constructor(
+        message: string, public readonly cause?: Error
+    ) {
+        super(message, 'NETWORK_ERROR');
+        this.name = 'NetworkError';
+        Object.setPrototypeOf(this, NetworkError.prototype);
+    }
+}
+
+export class TermMismatchError extends RaftError {
+    constructor(
+        public readonly expectedTerm: number,
+        public readonly actualTerm: number
+    ) {
+        super(`Term mismatch: expected ${expectedTerm}, got ${actualTerm}`, 'TERM_MISMATCH');
+        this.name = 'TermMismatchError';
+        Object.setPrototypeOf(this, TermMismatchError.prototype);
+    }
+}
+
+export class TimeoutError extends RaftError {
+    constructor(
+        message: string, public readonly cause?: Error
+    ) {
+        super(message, 'TIMEOUT');
+        this.name = 'TimeoutError';
+        Object.setPrototypeOf(this, TimeoutError.prototype);
+    }
+}
+
+export class InvalidStateError extends RaftError {
+    constructor(
+        public readonly fromState: string,
+        public readonly toState: string,
+    ) {
+        super(`Invalid state transition: from ${fromState} to ${toState}`, 'INVALID_STATE');
+        this.name = 'InvalidStateError';
+        Object.setPrototypeOf(this, InvalidStateError.prototype);
+    }
+}
+

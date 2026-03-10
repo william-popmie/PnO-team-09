bb51c784e9b6d7a64ab599c38b7d9604dccf1689
Wout Van Hemelrijck <woutvh2005@gmail.com>
2026-03-10 14:33:13 +0100

    eslint.config.mjs fix by adding it to ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint.config.mjs
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
@@ -8,50 +7,41 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-export default [
-  { ignores: ["**/build/", "**/coverage/"] },
-
-  // ✅ Don’t run the TS parser (esp. type-aware) on the ESLint config itself
-  {
-    files: ["eslint.config.mjs"],
-    languageOptions: { parserOptions: { project: null } },
-  },
-
-  ...compat.extends(
+export default [{
+    ignores: ["**/build/", "**/coverage/", "eslint.config.mjs"],
+}, ...compat.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-  ),
-
-  {
-    plugins: { "@typescript-eslint": typescriptEslint },
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
 
     languageOptions: {
-      parser: tsParser,
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
 
-      // ⚠️ Your current ecmaVersion/sourceType are for old JS.
-      // For TS projects, this is typically:
-      ecmaVersion: "latest",
-      sourceType: "module",
-
-      parserOptions: {
-        project: ["./tsconfig.json"],
-        tsconfigRootDir: __dirname,
-      },
+        parserOptions: {
+            project: ["./tsconfig.json"],
+        },
     },
 
     rules: {
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" }],
-      eqeqeq: ["error", "always"],
-      "guard-for-in": ["warn"],
-      "@typescript-eslint/prefer-for-of": ["warn"],
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+        }],
+
+        eqeqeq: ["error", "always"],
+        "guard-for-in": ["warn"],
+        "@typescript-eslint/prefer-for-of": ["warn"],
     },
-  },
-];
+}];
